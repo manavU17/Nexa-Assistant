0779a3adcffe79354b243d33f916111c901d4f5c
manavU17 <manavisintellegent@gmail.com>
2024-08-07 16:35:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import music_library
+from gtts import gTTS
+import pygame
+import os
+
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init() 
+newsapi = "28dcc927936e49568a2e76054a993fa4"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+    pygame.mixer.init()
+    pygame.mixer.music.load('temp.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
+
+
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = music_library.music[song]
+        webbrowser.open(link)
+if __name__ == "__main__":
+    speak("Initializing Nexa....")
+    while True:
+        r = sr.Recognizer()
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "nexa"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Nexa Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("Error; {0}".format(e))
+
+
